21378f85afd3e50876ae5f51df7af84cc99eb927
KenSinKan <demmax0512@proton.me>
2025-10-05 21:50:57 +0300

    direnv update

--- modules/programs/cli/direnv/default.nix
+++ modules/programs/cli/direnv/default.nix
@@ -5,6 +5,7 @@
     (_: {
       programs.direnv = {
         enable = true;
+        nix-direnv.enable = true;
         enableBashIntegration = true;
         enableZshIntegration = true;
         enableFishIntegration = false;
