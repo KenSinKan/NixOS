245f56b4ff328ba76759afc83e9a98901f2c0c65
KenSinKan <demmax0512@proton.me>
2025-10-07 14:54:05 +0300

    hardware update

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -74,6 +75,7 @@
           modules = [
             ./hosts/${host}/configuration.nix
           ];
+          # ++ lib.mkIf;
           specialArgs = {
             # inherit self inputs outputs;
             overlays = import ./overlays { inherit inputs host; };
