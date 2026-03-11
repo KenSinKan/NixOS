0786090efc27a3c358cfd6a1727fa48372aca775
KenSinKan <demmax0512@gmail.com>
2026-03-11 22:31:40 +0300

    Added zeditor

--- hosts/Lenovo-16AHP9/configuration.nix
+++ hosts/Lenovo-16AHP9/configuration.nix
@@ -37,7 +37,7 @@ in
     ../../modules/programs/browser/${vars.browser} # Set browser defined in variables.nix
     ../../modules/programs/terminal/${vars.terminal} # Set terminal defined in variables.nix
     ../../modules/programs/editor/${vars.editor} # Set editor defined in variables.nix
-    ../../modules/programs/editor/helix
+    ../../modules/programs/editor/zed
     ../../modules/programs/cli/${vars.tuiFileManager} # Set file-manager defined in variables.nix
     ../../modules/programs/cli/tmux
     ../../modules/programs/cli/direnv
